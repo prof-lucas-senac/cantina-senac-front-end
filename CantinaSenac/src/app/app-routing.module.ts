8ecf9a186c4837ae4c50b2a2f87b8f72d8627e73
ArthurRF632 <iajava159@gmail.com>
2024-10-16 21:08:32 -0300

    Criação das rotas das paginas

--- CantinaSenac/src/app/app-routing.module.ts
+++ CantinaSenac/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "feedbacks",
+    component: FeedbackComponent
+  },
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
